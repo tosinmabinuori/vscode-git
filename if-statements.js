37daffd9ee72f72cfbb238dab781569956aea665
tosinmabinuori <54864459+tosinmabinuori@users.noreply.github.com>
2019-09-16 18:15:36 +0000

    Add files via upload

---
+++ if-statements.js
@@ -0,0 +1,12 @@
+let fruit = 'orange';
+{console.log('The fruit name has more than five characters.');
+
+}
+
+// better way of writing the code //
+const fruit = 'orange';
+if (fruit.lenght > 5) {
+    console.log('The fruit name has more than five characters.');
+} else {
+    console.log ('The fruit name has five characters or less.');
+}
